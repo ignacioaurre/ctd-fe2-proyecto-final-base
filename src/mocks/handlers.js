f4f84064540e80e4b487f5d5be4100e2515be51b
ignacioaurrecoechea <ignacio.aurrecoechea@mercadolibre.com>
2022-07-06 17:12:18 -0300

    Cambiada la quote de Homero

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -6,11 +6,11 @@ export const handlers = [
       if (character) {
         return res(
           ctx.json( [
-              {
-                    quote: "Shoplifting is a victimless crime, like punching someone in the dark.",
-                    character: "Homer Simpson",
-                    image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
-                    characterDirection: "Left"
+            {
+              "quote": "I believe the children are the future... Unless we stop them now!",
+              "character": "Homer Simpson",
+              "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+              "characterDirection": "Right"
               },
             ],
           )
